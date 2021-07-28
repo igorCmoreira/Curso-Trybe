8b4ec7d467818277597f60207827c75b2ade51cf
Igor Campos Moreira <igorcampos200@gmail.com>
2021-07-28 17:37:52 -0300

    Pratica parte1

---
+++ Fundamentos/Bloco4-introdução-à-JavaScript-e-lógica-de-programação/Dia1_JavaScript-Primeiros_passos/Pratica/Parte1/script.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 0;
+
+
+console.log("o resultado de A + B =",  a+b);
+console.log("o resultado de A - B =", a-b);
+console.log("o resultado de A x B =", a*b);
+if (b === 0){
+  console.log("a divisão não é possivel")
+} else{
+  console.log("o resultado de A/B", a/b);
+}
