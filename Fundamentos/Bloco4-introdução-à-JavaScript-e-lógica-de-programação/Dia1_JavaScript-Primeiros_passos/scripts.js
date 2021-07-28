feddce20dbc559cec09ab8b2cf1582c5b88a89e8
Igor Campos Moreira <igorcampos200@gmail.com>
2021-07-28 16:02:08 -0300

    Parte 1

---
+++ Fundamentos/Bloco4-introdução-à-JavaScript-e-lógica-de-programação/Dia1_JavaScript-Primeiros_passos/scripts.js
@@ -0,0 +1,14 @@
+const myName = "igor";
+const birthCity = "Belo Horizonte";
+let birthYear = 2000;
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// No exercício pede para alterarmos a variável birthCity mas como ela é uma constante a sua alteração não é possível por este motivo aparece a mensagem de erro
